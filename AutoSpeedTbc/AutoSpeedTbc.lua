df0d6d7c471f185539ff685de3e1dc1f697ee0ca
darkwingducks <daniel.w.domikaitis@gmail.com>
2021-07-26 11:32:12 -0700

    Logo changed to red running boots with lightning bolts

--- AutoSpeedTbc/AutoSpeedTbc.lua
+++ AutoSpeedTbc/AutoSpeedTbc.lua
@@ -294,7 +294,7 @@ SlashCmdList["AUTOCARROT"] = function(msg)
 end
 
 autoSpeedButton = CreateFrame("Button", "autoSpeedButton", UIParent, "ActionButtonTemplate")
-autoSpeedButton.icon:SetTexture(298590)
+autoSpeedButton.icon:SetTexture(132536)
 autoSpeedButton:SetPoint("CENTER")
 autoSpeedButton.overlay = autoSpeedButton:CreateTexture(nil, "OVERLAY")
 autoSpeedButton.overlay:SetAllPoints(autoSpeedButton)
